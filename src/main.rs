5ef0154185ab52d819c291eb0a9993f2644fa892
caleb <16196262+spookyuser@users.noreply.github.com>
2024-03-24 16:32:24 +0200

    Add build script

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,37 @@
 use clap::{Arg, Command};
-
 use std::fs::File;
 use std::io::{Result, Write};
 use walkdir::WalkDir;
 
 // Configuration
-const STANDARD_EXCLUSIONS: [&str; 6] = [
+const STANDARD_EXCLUSIONS: [&str; 24] = [
     "node_modules/",
+    "metrics",
+    "sentry",
+    "luaScripts",
+    "lua",
+    "tsconfig",
+    "tracing",
+    "node_modules",
     ".git/",
     ".DS_Store",
     "dist/",
     "build/",
     "*.log",
+    ".turbo",
+    "fixtures",
+    "scripts",
+    "sql",
+    "yaml",
+    "yaml",
+    "l10n",
+    "lock",
+    "email",
+    "nestjs",
+    "speed-trap",
 ];
+
 const CUSTOM_INCLUDES: [&str; 3] = ["package.json", "../../README.md", "README.md"];
-const EXTENSIONS: [&str; 2] = [".ts", ".tsx"];
 
 fn main() -> Result<()> {
     // Define command-line arguments with clap
@@ -40,6 +57,20 @@ fn main() -> Result<()> {
                 .required(false)
                 .help("Name of the package to include in the output file headers"),
         )
+        .arg(
+            Arg::new("extensions")
+                .short('e')
+                .long("extensions")
+                .value_name("EXTENSIONS")
+                .help("Comma-separated list of file extensions to include (e.g., '.ts,.tsx')")
+                .value_parser(clap::builder::NonEmptyStringValueParser::new()),
+        )
+        .arg(
+            Arg::new("all_extensions")
+                .short('a')
+                .long("all")
+                .help("Include files with any extension"),
+        )
         .get_matches();
 
     let default_package_name = "".to_string();
@@ -49,6 +80,12 @@ fn main() -> Result<()> {
         .get_one::<String>("package_name")
         .unwrap_or(&default_package_name);
 
+    let all_extensions = matches.contains_id("all_extensions");
+    let extensions: Vec<&str> = match matches.get_one::<String>("extensions") {
+        Some(ext_str) => ext_str.split(',').collect(),
+        None => vec![],
+    };
+
     // Create output file
     let mut output_file = File::create(output_path)?;
 
@@ -56,13 +93,12 @@ fn main() -> Result<()> {
     for entry in WalkDir::new(repo_path)
         .into_iter()
         .filter_map(|e| e.ok())
-        .filter(|e| is_included(e.path()))
+        .filter(|e| is_included(e.path(), &extensions, all_extensions))
     {
         let path = entry.path();
         if path.is_file() {
             // Read and write file content
             let content = std::fs::read_to_string(path)?;
-
             writeln!(
                 output_file,
                 "===== BEGIN {prefix}/{path} =====",
@@ -78,8 +114,8 @@ fn main() -> Result<()> {
             )?;
         }
     }
-    // Run tree command from the command line
 
+    // Run tree command from the command line
     let output = std::process::Command::new("tree").arg(repo_path).output();
     match output {
         Ok(output) => {
@@ -97,9 +133,8 @@ fn main() -> Result<()> {
 }
 
 // Function to check if a file should be included
-fn is_included(path: &std::path::Path) -> bool {
+fn is_included(path: &std::path::Path, extensions: &[&str], all_extensions: bool) -> bool {
     let path_str = path.to_str().unwrap_or("");
-
     // Check for exclusions
     if STANDARD_EXCLUSIONS
         .iter()
@@ -112,5 +147,6 @@ fn is_included(path: &std::path::Path) -> bool {
     CUSTOM_INCLUDES
         .iter()
         .any(|inclusion| path_str.contains(inclusion))
-        || EXTENSIONS.iter().any(|ext| path_str.ends_with(ext))
+        || all_extensions
+        || extensions.iter().any(|ext| path_str.ends_with(ext))
 }
